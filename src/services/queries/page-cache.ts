b03be65801975d6b6f9c7f41152d229d8088ff1a
ragusrinivasan <raguraam.s@outlook.com>
2025-01-17 20:16:21 +0530

    page-cache-keys

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,16 @@
-export const getCachedPage = (route: string) => {};
+import { pageCacheKey } from "$services/keys";
+import { client } from "$services/redis";
 
-export const setCachedPage = (route: string, page: string) => {};
+const cacheRoutes = [
+    '/about', '/privacy', '/auth/signin', '/auth/signup'
+]
+
+export const getCachedPage = (route: string) => {
+    return cacheRoutes.includes(route) ? client.get(pageCacheKey(route)) : null
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    return cacheRoutes.includes(route) ? client.set(pageCacheKey(route), page, {
+        EX: 2
+    }) : null
+};
